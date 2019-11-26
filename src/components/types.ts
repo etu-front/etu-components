b5df47e813389959cb8b68f28c7f12264ede369a
handylee <lihandi@gmail.com>
2019-11-26 19:13:27 +0800

    BaseProps add onClick

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,6 +1,7 @@
-import { CSSProperties } from 'react'
+import { CSSProperties, MouseEvent } from 'react'
 
 export interface BaseProps {
   className?: string
   style?: CSSProperties
+  onClick?: (e?: MouseEvent<HTMLElement>) => any
 }
