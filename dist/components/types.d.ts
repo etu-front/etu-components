b849c50a289cb80072f258bf5b81556a3c2b6077
handylee <lihandi@gmail.com>
2019-11-26 19:13:43 +0800

    📦 bump version 1.0.2

--- dist/components/types.d.ts
+++ dist/components/types.d.ts
@@ -1,5 +1,6 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, MouseEvent } from 'react';
 export interface BaseProps {
     className?: string;
     style?: CSSProperties;
+    onClick?: (e?: MouseEvent<HTMLElement>) => any;
 }
