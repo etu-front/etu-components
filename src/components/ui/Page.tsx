f23431e5b71960e9d52710b4dc87f9ac2f85a721
handylee <lihandi@gmail.com>
2020-09-27 10:58:31 +0800

    ref: Page 组件导出 PageProps

--- src/components/ui/Page.tsx
+++ src/components/ui/Page.tsx
@@ -7,19 +7,19 @@ const Container = styled.div`
   min-height: 100vh;
   padding-bottom: env(safe-area-inset-bottom);
 `
-interface PageProps extends BaseProps {
+export interface PageProps {
   title?: string
   backgroundColor?: string
   paddingBottom?: number
   resetScroll?: boolean
 }
-const Page: FC<PageProps> = ({
+const Page: FC<PageProps & BaseProps> = ({
   backgroundColor = 'white', paddingBottom, resetScroll, style, className, children, title
 }) => {
   useEffect(() => {
     if (!resetScroll) return
     window.scrollTo(0, 0)
-  })
+  }, [resetScroll])
   return (
     <Container style={{ ...style, backgroundColor, paddingBottom }} className={className}>
       {typeof title !== 'undefined' && <DocumentTitle title={title} />}
