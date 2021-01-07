7ee7a981dc11639e4d1ac761ecde7f5a707b24e4
handylee <lihandi@gmail.com>
2021-01-07 09:06:18 +0800

    ref: remove LongPress

--- dist/components/ui/LongPress.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface ILongPressProps {
-    children: any;
-    callback: () => void;
-    ms?: number;
-}
-export default function LongPress({ children, callback, ms }: ILongPressProps): JSX.Element;
-export {};
