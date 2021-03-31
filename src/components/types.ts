dad3cdc92688bd7bc7866d7c51d88a05293794d5
handylee <lihandi@gmail.com>
2021-03-31 16:30:05 +0800

    ref: View 增加 id 属性

--- src/components/types.ts
+++ src/components/types.ts
@@ -4,6 +4,7 @@ export type MouseHandler = (e?: MouseEvent<HTMLElement | undefined>) => any
 
 export interface BaseProps {
   className?: string
+  id?: string
   style?: CSSProperties
   onClick?: MouseHandler
 }
