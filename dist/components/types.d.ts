304a5b3c3a5fbc466bbad13c7ea1c1bbede805b8
handylee <lihandi@gmail.com>
2021-03-31 16:30:25 +0800

    📦 bump version 2.1.1

--- dist/components/types.d.ts
+++ dist/components/types.d.ts
@@ -2,6 +2,7 @@ import { MouseEvent, CSSProperties } from 'react';
 export declare type MouseHandler = (e?: MouseEvent<HTMLElement | undefined>) => any;
 export interface BaseProps {
     className?: string;
+    id?: string;
     style?: CSSProperties;
     onClick?: MouseHandler;
 }
