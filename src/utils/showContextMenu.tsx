c458c7cbd88acb7e050a198980302ec9eed7f409
handylee <lihandi@gmail.com>
2021-06-21 11:36:20 +0800

    feat: 增加 showContextMenu 方法

---
+++ src/utils/showContextMenu.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const showContextMenu = (options: {
+  target: HTMLElement
+  x: number
+  y: number
+  component: React.ReactNode
+  className?: string
+  offset?: number
+}) => {
+  const { target, className = '', offset = 1 } = options
+
+  const dom = document.createElement('div')
+  dom.style.position = 'fixed'
+  dom.className = className
+  dom.style.visibility = 'hidden'
+  document.body.append(dom)
+  ReactDOM.render(<span>{options.component}</span>, dom)
+
+  let x = options.x + offset
+  let y = options.y + offset
+
+  const rect = target.getBoundingClientRect()
+
+  if (options.x + offset + dom.offsetWidth > rect.left + rect.width) {
+    x = options.x - dom.offsetWidth - offset
+  }
+  if (options.y + offset + dom.offsetHeight > rect.top + rect.height) {
+    y = options.y - dom.offsetHeight - offset
+  }
+
+  dom.style.left = `${x}px`
+  dom.style.top = `${y}px`
+  dom.style.visibility = 'visible'
+
+  let timer = 0
+
+  const clearTimer = () => window.clearTimeout(timer)
+
+  const destory = () => {
+    clearTimeout(timer)
+    dom.removeEventListener('mouseenter', clearTimer)
+    dom.removeEventListener('mouseleave', destory)
+    ReactDOM.unmountComponentAtNode(dom)
+    dom.remove()
+  }
+
+  const startTimer = () => window.setTimeout(destory, 1000)
+
+  timer = startTimer()
+  dom.addEventListener('mouseenter', clearTimer)
+  dom.addEventListener('mouseleave', destory)
+
+  return destory
+}
+
+export default showContextMenu
