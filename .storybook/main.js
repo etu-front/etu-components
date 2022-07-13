5ac37a0e37f3ceaae806d007d1fb6efdeff5da00
handy <lihandi@gmail.com>
2022-07-13 09:23:36 +0800

    remove unused storybook files

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/storybooks/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials"
-  ]
-}
