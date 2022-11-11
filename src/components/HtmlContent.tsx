53f290da259b953aba1728a85c592d81429a93f3
handy <lihandi@gmail.com>
2022-11-11 08:55:02 +0800

    💄 htmlcontent style

--- src/components/HtmlContent.tsx
+++ src/components/HtmlContent.tsx
@@ -20,20 +20,14 @@ export const Container = styled.div`
     line-height: 200%;
     text-align: justify;
   }
-  p:empty {
-    height: 1em;
-  }
+  p:empty { height: 1em;}
   p > img + br:last-child {
     display: none;
   }
   p > img:only-child {
-    margin: 0.5em 0;
     display: block;
   }
-  p > img {
-    margin: 0.5em 0;
-  }
-  img {
+  img, video {
     max-width: 100%;
   }
   table.table-bordered {
